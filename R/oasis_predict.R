0e4686a1d45f892ee6fd2e58651582ce18c16ed5
Elizabeth Sweeney <emsweene@jhsph.edu>
2015-05-18 10:28:09 -0400

    update oasis_predict and oasis_preproc

--- R/oasis_predict.R
+++ R/oasis_predict.R
@@ -1,58 +1,57 @@
+#' @title OASIS Prediction
+#' @description
 #' @importFrom AnalyzeFMRI GaussSmoothArray
-#' @export
-oasis_predict <- (flair, ##the flair volume
-                  t1, ##the t1 volume  
-                  t2, ##the t2 volume
-                  pd, ##the pd volume
-                  brain_mask = NULL, ## the brain mask
-                  preproc = FALSE,
-                  normalize = TRUE,
-                  model = default_model 
-  )
-{
-  setClass(Class="study",
-           representation(
-             flair ="nifti",
-             t1 ="nifti",
-             t2 ="nifti",
-             pd ="nifti",
-             brain_mask = 'nifti')
-  )
-  oasis_study <- (new("study",
-             flair = flair,
-             t1 = t1,
-             t2 = t2,
-             pd = pd,
-             brain_mask = brain_mask))
-  
-  modalities <- c("flair", "t1", "t2", "pd")
+#' @param flair flair volume of class nifti
+#' @param t1 flair volume of class nifti
+#' @param t2 flair volume of class nifti
+#' @param pd flair volume of class nifti
+#' @param brain_mask brain mask of class nifti, if NULL a brain mask will be created using fsl BET
+#' @param preproc calls the oasis_preproc function and performs the necessary preprocessing steps for OASIS
+#' @param normalize option to perform z-score normalization of the image, should be TRUE unless you train model
+#' using an alternative normalization 
+#' @param model an object of class glm used to make the OASIS predictions 
+#' @export 
+oasis_predict <- (flair, ##flair volume of class nifti
+                  t1, ##t1 volume of class nifti
+                  t2, ##t2 volume of class nifti
+                  pd, ##pd volume of class nifti
+                  brain_mask = NULL, ##brain mask of class nifti
+                  preproc = FALSE, ##option to preprocess the data
+                  normalize = TRUE, ##option to normalize 
+                  model = default_model ##OASIS model of class glm
+  ) {
+  oasis_study <- list(flair = flair, t1 = t1, t2 = t2, pd = pd)
   
   ## the image preproceesing 
   if(preproc == TRUE){
-    images <- oasis_preproc(flair, t1, t2, pd)
+    images <- oasis_preproc(oasis_study$flair, oasis_study$t1, oasis_study$t2, oasis_study$pd)
+  }
+ 
+  ## create a brain mask  
+  if(brain_mask == NULL){
+    brain_mask <- fslbet(infile = oasis_study$t1, retimg = TRUE)
+    brain_mask <- brain_mask > 0
+    oasis_study$brain_mask <- datatyper(brain_mask, trybyte= TRUE)
+  } else {
+    oasis_study$brain_mask <- brain_mask
   }
 
-  
   ## the image normalization 
   if(normalize == TRUE){
-    for(i in 1:length(modalities)){
-      assign(modalities[i], zscore_img(get(modalities[i]), 
-                                       mask = brain_mask, margin = NULL))  
+    oasis_study[1:4] <- lapply(oasis_study[1:4], function (x) zscore_img(x,  oasis_study$brain_mask, margin = NULL))  
     }
-  }
+
+  ## smooth the images using fslsmooth from the fslr package 
+    oasis_smooth_10 <- lapply(oasis_study[1:4], function(x) fslsmooth(x, sigma = 10, mask = brain_mask))
+    oasis_smooth_20 <- lapply(oasis_study[1:4], function(x) fslsmooth(x, sigma = 20, mask = brain_mask))
   
+  do.call(rbind.data.frame,oasis_study[1:4], oasis_smooth_10,  oasis_smooth_20)
   ## create a dataframe to store the oasis covariates 
   oasis.data <-  data.frame(matrix(NA, length(c(flair)), 12, 
                                    dimnames=list(c(), c(modalities,  
                                                         paste0(modalities, "_10"), 
                                                         paste0(modalities, "_20")))))
-  ## create oasis covariates and add these to the dataframe
-  for(i in 1:length(modalities)){
-    modal <- get(modalities[i])
-    oasis.data[,i] <- c(modal)
-    oasis.data[,c(i + 4)] <- c(fslsmooth(modal, sigma = 10, mask = brain_mask))
-    oasis.data[,c(i + 8)] <- c(fslsmooth(modal, sigma = 20, mask = brain_mask))
-  }
+
   
   ## make the model predictions 
   predictions <- 
--- R/oasis_preproc.R
+++ R/oasis_preproc.R
@@ -1,22 +1,41 @@
-#' @import fslr
-#' 
-oasis_preproc <- function(flair, t1, t2, pd){
+#' @title OASIS Image Preprocessing 
+#' @description This function does the required preprocessing for OASIS for the flair, t2, 
+#' t1, and pd volumes using fsl through fslr.  The preprcoessing steps are (1) inhomogenity
+#' correct using fsl fast, (2) rigid registration using fsl flirt, (3) creating
+#' a brain mask using fsl BET.  The function returns a list with the inhomogenity corrected
+#' volumes and the brain mask. 
+#' @param flair flair volume of class nifti
+#' @param t1 flair volume of class nifti
+#' @param t2 flair volume of class nifti
+#' @param pd flair volume of class nifti
+#' @return List og objects of class nifti 
+#' @examples \dontrun{
+#' ## when you write the dontrun, it doesn't run but will
+#' ## include it in the Rd file (documentation)
+#' }
+#' @import AnalyzeFMRI 
+oasis_preproc <- function(flair, #flair volume of class nifti
+                          t1, # t1 volume of class nifti
+                          t2, # t2 volume of class nifti
+                          pd # pd volume of class nifti
+                          ){
   
-  study = list(flair = flair, t1 = t1, t2 = t2, pd = pd)
+  study <- list(flair = flair, t1 = t1, t2 = t2, pd = pd)
   
   ## inhomogenity correction for all four modalities using fsl bias correct
   study_inhomo <- lapply(study, function(x) fsl_biascorrect(x, retimg = TRUE))
 
   ##rigidly register to the flair, t2, and pd to the t1 using fsl flirt 
-  study_inhomo_reg <- lapply(study, function(x)  flirt(infile = x, omat = 'temp',
+  study_inhomo_reg <- lapply(study[c(1,3:4)], function(x)  flirt(infile = x, omat = 'temp',
                                                              reffile =   study$t1, 
                                                              retimg = TRUE,  dof = 6))
   
   ##create a brain mask using fsl BET 
-  brain_mask <- fslbet(infile = t1, retimg = TRUE, outfile = outfile)
+  brain_mask <- fslbet(infile = t1, retimg = TRUE)
   brain_mask <- brain_mask > 0
   study_inhomo_reg$brain_mask <- datatyper(brain_mask, trybyte= TRUE)
   
+  ##return a list with the preprocessed images and a brain mask 
   return(study_inhomo_reg)
 
 }
