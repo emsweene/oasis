cc00c72b1ee88f91e4c2b5e0cd2229c16b019d23
muschellij2 <muschellij2@gmail.com>
2015-09-08 13:35:59 -0400

    adding check_nifti arguments

--- R/oasis_preproc.R
+++ R/oasis_preproc.R
@@ -20,19 +20,26 @@ oasis_preproc <- function(flair, #flair volume of class nifti
                           t1, # t1 volume of class nifti
                           t2, # t2 volume of class nifti
                           pd # pd volume of class nifti
-                          ){
+){
   
   study <- list(flair = flair, t1 = t1, t2 = t2, pd = pd)
-  
+  study = check_nifti(study)
+
   ## inhomogenity correction for all four modalities using fsl bias correct
   study_inhomo <- lapply(study, function(x) fsl_biascorrect(x, retimg = TRUE))
-
+  
+  
   ##rigidly register to the flair, t2, and pd to the t1 using fsl flirt 
-  study_inhomo_reg <- lapply(study[c(1,3:4)], function(x)  flirt(infile = x, omat = 'temp',
-                                                             reffile =   study$t1, 
-                                                             retimg = TRUE,  dof = 6))
+  study_inhomo_reg <- lapply(study[c("flair","t2", "pd")], function(x)  {
+    tfile = tempfile(fileext = ".mat")
+    flirt(infile = x, omat = tfile,
+          reffile =   study$t1, 
+          retimg = TRUE,  dof = 6)
+    }
+    )
   
   ##return a list with the preprocessed images and a brain mask 
-  return(list(flair = study_inhomo_reg[[1]], t1 = study_inhomo[[2]],  t2 = study_inhomo_reg[[2]], pd = study_inhomo_reg[[3]]))
-
-}
+  return(list(flair = study_inhomo_reg[[1]], t1 = study_inhomo[[2]],  
+              t2 = study_inhomo_reg[[2]], pd = study_inhomo_reg[[3]]))
+  
+  }
