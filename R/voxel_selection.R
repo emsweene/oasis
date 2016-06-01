1f93a6d6a6ae7d18a6cd828c9c896d83918ab7de
muschellij2 <muschellij2@gmail.com>
2016-05-31 22:40:15 -0400

    Added new .travis.

--- R/voxel_selection.R
+++ R/voxel_selection.R
@@ -9,21 +9,25 @@
 #' @examples
 #' library(oasis)
 #' niis = tempfile(fileext = ".nii.gz")
-#' download.file(paste0("https://dl.dropbox.com/u/2785709/brainder", 
-#' "/software/flair/templates/GG-853-FLAIR-2.0mm.nii.gz"), 
-#'  destfile = niis)
-#' flair <- readnii(niis) 
-#' if (have.fsl()) {
-#' brain_mask = fslbet(niis) > 0
-#' } else {
-#'  ind = list(c(10L, 81L), c(12L, 101L), c(3L, 78L))
-#'  all.ind = lapply(ind, function(x) seq(x[1], x[2]))
-#'  brain_mask = niftiarr(flair, 0)
-#'  eg = expand.grid(all.ind)
-#'  eg = as.matrix(eg)
-#'  brain_mask[eg] = 1
+#' if (require(downloader)) {
+#'  download(paste0("https://dl.dropbox.com/u/2785709/brainder", 
+#'          "/software/flair/templates/GG-853-FLAIR-2.0mm.nii.gz"), 
+#'          destfile = niis)
+#'  
+#'  flair <- readnii(niis) 
+#'  if (have.fsl()) {
+#'    brain_mask = fslbet(niis) > 0
+#'  } else {
+#'    ind = list(c(10L, 81L), c(12L, 101L), c(3L, 78L))
+#'    all.ind = lapply(ind, function(x) seq(x[1], x[2]))
+#'    brain_mask = niftiarr(flair, 0)
+#'    eg = expand.grid(all.ind)
+#'    eg = as.matrix(eg)
+#'    brain_mask[eg] = 1
+#'  }
+#'  voxel_selection_mask <- voxel_selection(flair, 
+#'    brain_mask, cutoff = .85) 
 #' }
-#' voxel_selection_mask <- voxel_selection(flair, brain_mask, cutoff = .85) 
 #' @import oro.nifti
 #' @export
 voxel_selection <- function(flair, ##the flair volume
@@ -31,7 +35,7 @@ voxel_selection <- function(flair, ##the flair volume
                             cutoff ## the percentile cutoff for the thresholding 
                             ){
   stopifnot(length(cutoff) == 1)
-  arr = as.array(brain_mask)
+  arr = array(brain_mask, dim = dim(brain_mask))
   class(arr) = "numeric"
   stopifnot(all(c(arr) %in% c(0, 1)))
   ##find the value to threshold the flair volume at 
