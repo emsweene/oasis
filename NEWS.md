c749c7867f52017b1c8b75812e3566458b0494ba
muschellij2 <muschellij2@gmail.com>
2018-02-01 10:35:16 -0500

    Updated oasis.

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# oasis 3.0
+
+* Added the corrected `oasis_model` and `nopd_oasis_model` to the package
+
+* The original model from the paper is located at: `original_oasis_model`.
+
+* Added a `NEWS.md` file to track changes to the package.
--- R/oasis_model.R
+++ R/oasis_model.R
@@ -1,7 +1,27 @@
 #' @title OASIS glm Object 
 #'
 #' @description A \code{glm} object containing the OASIS model from the OASIS paper. 
+#' @name original_oasis_model
+#' @docType data
+#' @format A \code{glm} object 
+"original_oasis_model"
+
+
+#' @title Updated OASIS glm Object 
+#'
+#' @description A \code{glm} object containing the OASIS model from the updated, corrected
+#' data.  The original model from the OASIS paper is located at 
+#' \code{\link{original_oasis_model}}.
 #' @name oasis_model
 #' @docType data
 #' @format A \code{glm} object 
 "oasis_model"
+
+#' @title Updated OASIS glm Object 
+#'
+#' @description A \code{glm} object containing the OASIS model from the updated, corrected
+#' data. 
+#' @name nopd_oasis_model
+#' @docType data
+#' @format A \code{glm} object 
+"nopd_oasis_model"
